3d0c22ea834f87108b32cb5ac57d144b6975ba94
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-23 13:07:18 +0300

    Create base_api_repository.dart

---
+++ lib/src/data/datasource/remote/base/base_api_repository.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:retrofit/dio.dart';
+
+import '../../../../utils/resources/data_state.dart';
+
+abstract class BaseApiRepository {
+  @protected
+  Future<DataState<T>> getStateOf<T>({
+    required Future<HttpResponse<T>> Function() request,
+  }) async {
+    try {
+      final httpResponse = await request();
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        throw DioException(
+          response: httpResponse.response,
+          requestOptions: httpResponse.response.requestOptions,
+        );
+      }
+    } on DioException catch (error) {
+      return DataFailed(error);
+    }
+  }
+}

2f6b1df9108b8eca8fa8053d794e35d4fbdbad2a
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-23 13:07:52 +0300

    Create strings.dart

---
+++ lib/src/utils/constants/strings.dart
@@ -0,0 +1,10 @@
+// App
+const String appTitle = 'Flutter Clean Architecture';
+
+// Networking and APIs
+const String baseUrl = 'https://newsapi.org/v2';
+const String defaultApiKey = '06e452c0393240b8981e7155d132c75b';
+
+// Storage and Databases
+const String articlesTableName = 'article_table';
+const String databaseName = 'app_database.db';

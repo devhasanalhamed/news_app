b780d827591c7f3a42cfd5eea360a7ee18506672
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-23 13:07:11 +0300

    Update app_router.dart

--- lib/src/config/router/app_router.dart
+++ lib/src/config/router/app_router.dart
@@ -1,2 +1,14 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
+
+import '../../presentation/views/breaking_news_view.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes =>
+      [AutoRoute(page: BreakingNewsView.page, initial: true)];
+}
+
+final appRouter = AppRouter();

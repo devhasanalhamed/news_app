03c9735a4c8d08c72fdda78a1f2e699ce350d90a
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-23 13:07:47 +0300

    Create breaking_news_view.dart

---
+++ lib/src/presentation/views/breaking_news_view.dart
@@ -0,0 +1,85 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'package:ionicons/ionicons.dart';
+import '../../domain/model/article.dart';
+import '../../utils/extenstions/scroll_controller.dart';
+import '../cubits/remote_articles_cubit.dart';
+
+@RoutePage()
+class BreakingNewsView extends HookWidget {
+  const BreakingNewsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final remoteArticlesCubit = BlocProvider.of<RemoteArticlesCubit>(context);
+    final scrollController = useScrollController();
+
+    useEffect(() {
+      // scrollController.addListener(() {
+      //   if (scrollController.position.pixels ==
+      //       scrollController.position.maxScrollExtent) {
+      //     remoteArticlesCubit.getBreakingNewsArticles();
+      //   }
+      // });
+      scrollController.onScrollEndsListener(() {
+        remoteArticlesCubit.getBreakingNewsArticles();
+      });
+      return scrollController.dispose;
+    }, const []);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Daily News',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: BlocBuilder<RemoteArticlesCubit, RemoteArticlesState>(
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case RemoteArticlesLoading:
+              return const Center(child: CircularProgressIndicator());
+            case RemoteArticlesFailed:
+              return const Center(child: Icon(Ionicons.refresh));
+            case RemoteArticlesSuccess:
+              return _buildArticles(
+                scrollController,
+                state.articles,
+                state.noMoreData,
+              );
+            default:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildArticles(
+    ScrollController scrollController,
+    List<Article> articles,
+    bool noMoreData,
+  ) {
+    return CustomScrollView(
+      controller: scrollController,
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) => ArticleWidget(article: articles[index]),
+            childCount: articles.length,
+          ),
+        ),
+        if (!noMoreData)
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(top: 14, bottom: 32),
+              child: CircularProgressIndicator(),
+            ),
+          ),
+      ],
+    );
+  }
+}

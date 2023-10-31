cc6879ffbf754dbe71d94cde7be64cdd1019de41
Hasan Al-Hamed <81860602+devhasanalhamed@users.noreply.github.com>
2023-11-01 01:38:41 +0300

    Update strings.dart

--- lib/src/utils/constants/strings.dart
+++ lib/src/utils/constants/strings.dart
@@ -3,7 +3,7 @@ const String appTitle = 'Flutter Clean Architecture';
 
 // Networking and APIs
 const String baseUrl = 'https://newsapi.org/v2';
-const String defaultApiKey = '06e452c0393240b8981e7155d132c75b';
+const String defaultApiKey = '****';
 
 // Storage and Databases
 const String articlesTableName = 'article_table';
